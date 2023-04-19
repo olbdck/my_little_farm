af9277d82cf1710406449ca448e725c8a6f58cd8
Oleg Badychka <lilith@Lilith.local>
2023-04-20 00:24:31 +0300

    from Farmers model remove Phone field

--- my_little_farm/members/admin.py
+++ my_little_farm/members/admin.py
@@ -3,6 +3,6 @@ from .models import Farmers
 
 # Register your models here.
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ("firstname", "lastname", "joined_date",)
+    list_display = ("firstname", "lastname", "cows_in_own", "joined_date",) 
     
 admin.site.register(Farmers, MemberAdmin)
